de785c920f23694653cd275d868002fed4906d69
thijsbenschop <thijsbenschop@gmail.com>
2016-11-14 14:07:35 +0100

    Correct quantlets regarding PAV (932 and 933)

--- QID-933-MVAMDSpooladj/MVAMDSpooladj.r
+++ QID-933-MVAMDSpooladj/MVAMDSpooladj.r
@@ -3,6 +3,15 @@
 rm(list = ls(all = TRUE))
 graphics.off()
 
+# install and load packages
+libraries = c("Iso")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)
+})
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+
+# Distance matrix with prior ordering of distances (delta_ij)
+# for cities Berlin, Dresden, Hamburg, Koblemz, Muenchen, Rostock
 ber = c(0, 2, 4, 12, 11, 3)
 dre = c(2, 0, 6, 10, 7, 5)
 ham = c(4, 6, 0, 8, 15, 1)
@@ -12,6 +21,7 @@ ros = c(3, 5, 1, 13, 14, 0)
 
 dist = rbind(ber, dre, ham, kob, mue, ros)
 
+# Determine x, the coordinate matrix of the cities
 # a,b,h,i are matrices
 a = (dist^2) * (-0.5)
 i = diag(6)
@@ -22,9 +32,9 @@ e = eigen(b)
 
 g1  = cbind(e$vectors[, 1], e$vectors[, 2])
 g2  = diag(e$values[1:2])
-x   = g1 %*% (g2^0.5)       # Determine the coordinate matrix
+x   = g1 %*% (g2^0.5)        # Determine the coordinate matrix
 
-# Determine the dissimilarities
+# Determine the distances between the cities from their coordinates (d_ij)
 d12 = ((x[1, 1] - x[2, 1])^2 + (x[1, 2] - x[2, 2])^2)^0.5
 d13 = ((x[1, 1] - x[3, 1])^2 + (x[1, 2] - x[3, 2])^2)^0.5
 d14 = ((x[1, 1] - x[4, 1])^2 + (x[1, 2] - x[4, 2])^2)^0.5
@@ -45,29 +55,19 @@ d46 = ((x[4, 1] - x[6, 1])^2 + (x[4, 2] - x[6, 2])^2)^0.5
 
 d56 = ((x[5, 1] - x[6, 1])^2 + (x[5, 2] - x[6, 2])^2)^0.5
 
-d14 = (d14 + d15)/2
-d15 = d14
-d16 = (d14 + d15 + d16 + d23 + d24 + d25 + d26)/7
-d14 = d16
-d15 = d16
-d23 = d16
-d24 = d16
-d25 = d16
-d26 = d16
-d35 = (d35 + d36)/2
-d36 = d35
-
+# Create distance matrix
 dd = rbind(c(0, d12, d13, d14, d15, d16), c(d12, 0, d23, d24, d25, d26), c(d13, 
-    d23, 0, d34, d35, d36), c(d14, d24, d34, 0, d45, d46), c(d15, d25, d35, d45, 
-    0, d56), c(d16, d26, d36, d46, d56, 0))
-
-f = cbind(c(1:15), c(d12, d13, d14, d15, d16, d23, d24, d25, d26, d34, d35, d36, 
-    d45, d46, d56))
-
-l = cbind(c(15, 1), c(14.4, 3.17))
+                                                                           d23, 0, d34, d35, d36), c(d14, d24, d34, 0, d45, d46), c(d15, d25, d35, d45, 
+                                                                                                                                    0, d56), c(d16, d26, d36, d46, d56, 0))
+# Order the distance based on order in prior order in dist
+f = cbind(c(1:15), c(d36, d12, d16, d13, d26, d23, d25, d34, d45, d24, d15, d14, d46, d56, d35))
+  
+# Use PAV (from PAVA package)
+f <- cbind(f, pava(f[,2]))
+l = cbind(c(15, 1), c(f[15,3], f[1,3]))
 
 # Plot
-plot(f, xlim = c(0, 16), xlab = "Rank", ylab = "Distance", main = "Pool-Adjacent-Violator-Algorithm", 
+plot(f[,c(1,3)], xlim = c(0, 16), ylim = c(0,16), xlab = "Dissimilarity rank", ylab = "Distance", main = "Pool-Adjacent-Violator-Algorithm", 
     cex.axis = 1.2, cex.lab = 1.2, cex.main = 1.8)
-lines(f, lty = 2, lwd = 3, col = "blue")
+lines(f[,c(1,3)], lty = 2, lwd = 3, col = "blue")
 segments(l[1, 1], l[1, 2], l[2, 1], l[2, 2], lwd = 3, col = "blue") 
