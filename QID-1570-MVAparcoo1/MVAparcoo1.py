1883da3d533632088943728d7c3e276d01d71ced
LMGV <57717914+LMGV@users.noreply.github.com>
2020-04-22 10:50:18 +0200

    Add files via upload

---
+++ QID-1570-MVAparcoo1/MVAparcoo1.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("bank2.dat", sep = "\s+", header=None)
+x = data[95:105]
+
+c = np.ravel([[1]*5, [2]*5])
+
+y = (x - x.min())/(x.max() - x.min())
+y.loc[:, 6] = c
+y.columns = ["1", "2", "3", "4", "5", "6", "c"]
+
+
+
+fig, ax = plt.subplots(figsize = (15, 10))
+pd.plotting.parallel_coordinates(y, "c", color = ["black", "r"])
+ax.legend().set_visible(False)
+plt.title("Parallel coordinates plot (Bank data)")
+
+plt.show()
+
