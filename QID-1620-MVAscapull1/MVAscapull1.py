72e222ecec0811211e028e7fce28051183202f41
LMGV <57717914+LMGV@users.noreply.github.com>
2021-01-06 20:12:25 +0300

    Python code

---
+++ QID-1620-MVAscapull1/MVAscapull1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+x = pd.read_csv("pullover.dat", sep = "\s+", header=None)
+
+fig, ax = plt.subplots(figsize = (10, 10))
+ax.scatter(x.iloc[:,1], x.iloc[:,0], c = "w", edgecolors = "black")
+plt.xlim(78, 127)
+plt.ylim(80, 240)
+plt.xlabel("Price (X2)", fontsize = 14)
+plt.ylabel("Sales (X1)", fontsize = 14)
+plt.title("Pullovers Data", fontsize = 14)
+plt.show()
